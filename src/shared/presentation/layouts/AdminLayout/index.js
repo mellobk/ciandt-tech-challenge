988a09a133ad6e83919a372b7fc728b31a18387f
mellobk <65514817+mellobk@users.noreply.github.com>
2022-07-17 12:57:05 -0500

    add menu

--- src/shared/presentation/layouts/AdminLayout/index.js
+++ src/shared/presentation/layouts/AdminLayout/index.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import "./AdminLayout.scss";
 
-
-const AdminLayout = ({children}) => {
-
+const AdminLayout = ({ children }) => {
   return (
-  <div className="admin-layout">
-    {children}
-  </div>
+    <div className="admin-layout">
+      <div className="menu">
+        {menu.map((item, index) => {
+          <Link
+            key={index}
+            onClick={() => toggleAction(key)}
+            to={item.link}
+            className="layout-link"
+          >
+            {item.icon}
+          </Link>;
+        })}
+      </div>
+      <div>{children}</div>
+    </div>
   );
 };
 
